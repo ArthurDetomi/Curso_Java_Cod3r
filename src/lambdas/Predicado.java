f8853f97d7c6a055c4e4ff47ab95759770ba02ff
Arthur Detomi <arthurdetomi@gmail.com>
2023-01-19 17:19:18 -0300

    Espaçamento errado

--- src/lambdas/Predicado.java
+++ src/lambdas/Predicado.java
@@ -8,7 +8,7 @@ public class Predicado {
         Predicate<Produto> isCaro = produto ->
                 ((produto.preco * (1 - produto.desconto)) >= 750.0);
 
-        Produto produto = new Produto("Notebook", 3436.25,0.20);
+        Produto produto = new Produto("Notebook", 3436.25, 0.20);
         System.out.println(isCaro.test(produto));
     }
 
