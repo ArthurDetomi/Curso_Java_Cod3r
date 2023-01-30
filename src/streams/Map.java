18201b9b1781f9a41f2946ba8af1ac82a404a796
Arthur Detomi <arthurdetomi@gmail.com>
2023-01-30 17:05:47 -0300

    Map #02

---
+++ src/streams/Map.java
@@ -0,0 +1,30 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
+
+public class Map {
+
+    public static void main(String[] args) {
+        Consumer<String> println = System.out::println;
+
+        List<String> marcas = Arrays.asList("BMW", "Audi", "Honda");
+        // forEach é uma função terminal
+        marcas.stream().map(m -> m.toUpperCase()).forEach(println);
+
+        // UnaryOperator<String> maiuscula = n -> n.toUpperCase();
+        UnaryOperator<String> primeiraLetra = n -> n.charAt(0) + "";
+         // UnaryOperator<String> grito = n -> n + "!!!";
+
+        System.out.println("Usando composição");
+        marcas.stream()
+                .map(Utilitarios.maiuscula)
+                .map(primeiraLetra)
+                .map(Utilitarios::grito)
+                .forEach(println);
+
+    }
+
+}
---
+++ src/streams/Utilitarios.java
@@ -0,0 +1,13 @@
+package streams;
+
+import java.util.function.UnaryOperator;
+
+public interface Utilitarios {
+
+    public static UnaryOperator<String> maiuscula = n -> n.toUpperCase();
+    public static UnaryOperator<String> primeiraLetra = n -> n.charAt(0) + "";
+    public static String grito (String n) {
+        return n + "!!!";
+    }
+
+}
